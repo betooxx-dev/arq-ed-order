e2a0a358f1bc13af34def8cb63b1f4d8fc4d006a
evilBetooxx <albertof4in@gmail.com>
2024-04-18 11:10:53 -0600

    Urls

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.disable("x-powered-by");
 
 app.use(express.json());
-app.use(cors({ origin: 'https://comfy-cranachan-cb3bd1.netlify.app/', 
+app.use(cors({ origin: 'https://comfy-cranachan-cb3bd1.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD', 'PREFLIGHT', 'CONNECT'],
     credentials: true,
 }));
